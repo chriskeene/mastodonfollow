362df8f1e89f4f39314f276463bdf6ba144a0249
Chris Keene <chriskeene@gmail.com>
2022-11-18 18:07:31 +0000

    Update mastodonfollow.js

--- mastodonfollow.js
+++ mastodonfollow.js
@@ -1,5 +1,5 @@
 javascript:q=location.href;
-const usplit = q.split('/');
+usplit = q.split('/');
 const u1 = usplit[usplit.length - 1];
 const s1 = usplit[usplit.length - 2];
 void(open('https://mastodon.social/' + u1 + '@' + s1));
